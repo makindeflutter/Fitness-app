c62d9b5ba964d056ac3b5d90742e97cb33bedd81
makindeflutter <proffakinyinka@gmail.com>
2024-01-06 10:37:14 +0100

    m

--- fitnessapp/lib/training/challenge.dart
+++ fitnessapp/lib/training/challenge.dart
@@ -1,3 +1,4 @@
+import 'package:fitnessapp/discovery/with_equiment.dart';
 import 'package:fitnessapp/shared/styles/component.dart';
 import 'package:flutter/material.dart';
 
@@ -29,7 +30,12 @@ class _ChallengeState extends State<Challenge> {
           padding: const EdgeInsets.all(8.0),
           child: customTraincard(
               maintext: 'UPPPER BODY',
-              ontap: () {},
+              ontap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const Witheq();
+                }));
+              },
               images: const NetworkImage(
                 'https://images.unsplash.com/photo-1597452485669-2c7bb5fef90d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80',
               ),
